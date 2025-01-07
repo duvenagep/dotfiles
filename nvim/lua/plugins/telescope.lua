6a5226a4b84a80e953e1138a020278d72db1b12f
Paul Duvenage <duvenagep@gmail.com>
2025-01-07 17:47:39 +0100

    Feature/ghostty (#9)
    * feat(cars): Tmux-sessionizer
    
    * feat(dot): Updated dotfiles

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,44 +1,72 @@
 return {
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.5",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "BurntSushi/ripgrep",
-            "junegunn/fzf",
-        },
-        config = function()
-            local builtin = require("telescope.builtin")
-            vim.keymap.set("n", "<Leader>ff", builtin.find_files, {})
-            vim.keymap.set("n", "<Leader>pg", builtin.live_grep, {})
-        end,
-    },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"BurntSushi/ripgrep",
+			"junegunn/fzf",
+		},
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<Leader>ff", builtin.find_files, {})
+			vim.keymap.set("n", "<Leader>pg", builtin.live_grep, {})
+		end,
+	},
 
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-        -- "nvim-telescope/telescope-fzf-native.nvim",
-        config = function()
-            require("telescope").setup({
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({}),
-                    },
-                    -- ["fzf"] = {
-                    --     fuzzy = true,
-                    --     override_generic_sorter = true,
-                    --     override_file_sorter = true,
-                    --     case_mode = "smart_case",
-                    -- },
-                },
-            })
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		-- "nvim-telescope/telescope-fzf-native.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+					-- ["fzf"] = {
+					--     fuzzy = true,
+					--     override_generic_sorter = true,
+					--     override_file_sorter = true,
+					--     case_mode = "smart_case",
+					-- },
+				},
+			})
 
-            require("telescope").load_extension("ui-select")
-            -- require("telescope").load_extension("fzf")
-        end,
-    },
+			require("telescope").load_extension("ui-select")
+			-- require("telescope").load_extension("fzf")
+		end,
+	},
 
-    {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
-    },
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+	},
+	{
+		"PedramNavid/dbtpal",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		ft = {
+			"sql",
+			"md",
+			"yaml",
+		},
+		keys = {
+			{ "<leader>drf", "<cmd>DbtRun<cr>" },
+			{ "<leader>drp", "<cmd>DbtRunAll<cr>" },
+			{ "<leader>dtf", "<cmd>DbtTest<cr>" },
+			{ "<leader>dm", "<cmd>lua require('dbtpal.telescope').dbt_picker()<cr>" },
+		},
+		config = function()
+			require("dbtpal").setup({
+				path_to_dbt = "dbt",
+				path_to_dbt_project = "",
+				path_to_dbt_profiles_dir = vim.fn.expand("~/.dbt"),
+				extended_path_search = true,
+				protect_compiled_files = true,
+			})
+			require("telescope").load_extension("dbtpal")
+		end,
+	},
 }
